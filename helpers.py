726893e441c59464bf2744bc0370c78af37d425c
Matt J Ashworth <MattJAshworth@users.noreply.github.com>
2019-03-08 11:31:28 +0000

    Updated FB API code

--- helpers.py
+++ helpers.py
@@ -8,18 +8,15 @@ app_key = '50f56416e1297dfd609b3a7b2ef1ef90'
 access_token = 'EAAFXXOMofxcBAIcctnErMVmOTZB2riOf1fDy6rrXUPYZBnPeujHHDP5rpK5xz5FG3vEiQcxTZBKNYAiIaafrDsrPBmAYnl4SYVYZAJN7te1ATLpuZBjVXTUkPeFwV2tLQb7juZBGxdMwXxfM6WJeVBIIDZA48ZAkBAHyUyZCyjZAg5CwZDZD'
 
 
-def fbpost(msg):
+def fbpost(msg, img):
 
-  cfg = {
-    "page_id"      : os.environ["843586662650752"],  
-    "access_token" : os.environ["EAAFLj5ZAN3pcBAJ49pazCZAQS7FPwovXpYE10zoLd7l7V1ncYynq2EbfMstMBwr2geqywSedbQ15KfAvoCeuL480DTRgrXEinejCVR9PEnnqmLoQn3WXIVp2Ane33KGKZCcc0CpKfJZBXZBZBbmSMsZCO0gy4XRNN9hbFistww71dKVVZB6SZAJWiDoraZBUko4O6ZCf68dWd5XacbOZB6y19Y9x"]
-    }
 
   # Post to Facebook
-try:
-    graph = facebook.GraphAPI(access_token=access_token)
+  try:
+    graph = facebook.GraphAPI(access_token)
     #graph.put_wall_post(message=random_quote())
-    graph.put_photo(image=open("image.jpg", 'rb'),message=msg, profile_id='843586662650752')
-except Exception as e:
+    #graph.put_photo(image=open(img, 'rb'),message=msg, profile_id='843586662650752')
+    graph.put_object(parent_object='me', connection_name='feed', message=msg)
+  except Exception as e:
     print(e)
     pass
