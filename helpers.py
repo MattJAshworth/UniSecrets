3238e18fce0e372c8b154ba29d55d2e474dfe3c2
Matt J Ashworth <MattJAshworth@users.noreply.github.com>
2019-03-08 10:41:20 +0000

    Add files via upload

--- helpers.py
+++ helpers.py
@@ -19,8 +19,7 @@ def fbpost(msg):
 try:
     graph = facebook.GraphAPI(access_token=access_token)
     #graph.put_wall_post(message=random_quote())
-    graph.put_object(parent_object='843586662650752', connection_name='feed',
-                  message=msg)
+    graph.put_photo(image=open("image.jpg", 'rb'),message='Caption of the image', profile_id='843586662650752')
 except Exception as e:
     print(e)
     pass
