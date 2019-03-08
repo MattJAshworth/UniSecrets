ff7dec1761e972f1aed5046213e4b1aee8b01f92
Matt J Ashworth <MattJAshworth@users.noreply.github.com>
2019-03-08 10:15:16 +0000

    Updated FB API code

--- helpers.py
+++ helpers.py
@@ -2,6 +2,12 @@ import facebook
 import os
 
 
+# Constants
+app_id = '377531436400407'
+app_key = '50f56416e1297dfd609b3a7b2ef1ef90'
+access_token = 'EAAFXXOMofxcBAIcctnErMVmOTZB2riOf1fDy6rrXUPYZBnPeujHHDP5rpK5xz5FG3vEiQcxTZBKNYAiIaafrDsrPBmAYnl4SYVYZAJN7te1ATLpuZBjVXTUkPeFwV2tLQb7juZBGxdMwXxfM6WJeVBIIDZA48ZAkBAHyUyZCyjZAg5CwZDZD'
+
+
 def fbpost(msg):
 
   cfg = {
@@ -9,20 +15,12 @@ def fbpost(msg):
     "access_token" : os.environ["EAAFLj5ZAN3pcBAJ49pazCZAQS7FPwovXpYE10zoLd7l7V1ncYynq2EbfMstMBwr2geqywSedbQ15KfAvoCeuL480DTRgrXEinejCVR9PEnnqmLoQn3WXIVp2Ane33KGKZCcc0CpKfJZBXZBZBbmSMsZCO0gy4XRNN9hbFistww71dKVVZB6SZAJWiDoraZBUko4O6ZCf68dWd5XacbOZB6y19Y9x"]
     }
 
-  api = get_api(cfg)
-  status = api.put_object(parent_object='me', connection_name='feed', message=msg)
-
-def get_api(cfg):
-  graph = facebook.GraphAPI(cfg['access_token'])
-  # Get page token to post as the page. You can skip 
-  # the following if you want to post as yourself. 
-  resp = graph.get_object('me/accounts')
-  page_access_token = None
-  for page in resp['data']:
-    if page['id'] == cfg['page_id']:
-      page_access_token = page['access_token']
-  graph = facebook.GraphAPI(page_access_token)
-  return graph
-  # You can also skip the above if you get a page token:
-  # http://stackoverflow.com/questions/8231877/facebook-access-token-for-pages
-  # and make that long-lived token as in Step 3
+  # Post to Facebook
+try:
+    graph = facebook.GraphAPI(access_token=access_token)
+    #graph.put_wall_post(message=random_quote())
+    graph.put_object(parent_object='843586662650752', connection_name='feed',
+                  message=msg)
+except Exception as e:
+    print(e)
+    pass
